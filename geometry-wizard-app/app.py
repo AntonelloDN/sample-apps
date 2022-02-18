dcec861f626df4bd87b30de3047231ddc32a7916
Antonello Di Nunzio <plugin@antonellodinunzio.online>
2022-02-18 14:29:28 +0100

    fix(ux): use slider instead of input numbers

--- geometry-wizard-app/app.py
+++ geometry-wizard-app/app.py
@@ -26,100 +26,102 @@ st.image(image, caption=option)
 vertices = []
 
 if option == 'L-shape':
-    a = st.number_input(
-      "Segment A",
-      min_value=1.0,
-      max_value=9999999.0,
-      value=20.0, 
-      step=0.1,
-      format="%f",
-      key="l-shape-a")
-    b = st.number_input(
-      "Segment B",
-      min_value=1.0,
-      max_value=9999999.0,
-      value=8.0, 
-      step=0.1,
-      format="%f",
-      key="l-shape-b")
-    c = st.number_input(
-      "Segment B",
-      min_value=1.0,
-      max_value=9999999.0,
-      value=8.0, 
-      step=0.1,
-      format="%f",
-      key="l-shape-c")
-    d = st.number_input(
-      "Segment D",
-      min_value=1.0,
-      max_value=9999999.0,
-      value=20.0, 
-      step=0.1,
-      format="%f",
-      key="l-shape-d")
-    
-    # input validation
-    if a <= c:
-      c = a
-      st.warning('A must be > C')
-    if d <= b:
-      b = d
-      st.warning('D must be > B')
-    
-    vertices = [
-      Point2D(0,0), 
-      Point2D(a,0),
-      Point2D(a,b),
-      Point2D(c,b),
-      Point2D(c,d),
-      Point2D(0,d)
-    ]
+
+    with st.expander(option):
+        min_val = 1.0
+        max_val = 50.0
+
+        a = st.slider(
+        label="Segment A",
+        min_value=min_val,
+        max_value=max_val,
+        value=20.0, 
+        step=0.1,
+        key="l-shape-a")
+        b = st.slider(
+        "Segment B",
+        min_value=min_val,
+        max_value=max_val,
+        value=8.0, 
+        step=0.1,
+        key="l-shape-b")
+        c = st.slider(
+        "Segment B",
+        min_value=min_val,
+        max_value=max_val,
+        value=8.0, 
+        step=0.1,
+        key="l-shape-c")
+        d = st.slider(
+        "Segment D",
+        min_value=min_val,
+        max_value=max_val,
+        value=20.0, 
+        step=0.1,
+        key="l-shape-d")
+        
+        # input validation
+        if a <= c:
+            c = a
+            st.warning('A must be > C')
+        if d <= b:
+            b = d
+            st.warning('D must be > B')
+        
+        vertices = [
+        Point2D(0,0), 
+        Point2D(a,0),
+        Point2D(a,b),
+        Point2D(c,b),
+        Point2D(c,d),
+        Point2D(0,d)
+        ]
 
 # generic info
-n_floors = st.number_input(
+n_floors = st.slider(
     "Floors Number",
     min_value=1,
-    max_value=999,
+    max_value=30,
     value=2, 
     step=1,
     key="num-floor")
 
-height = st.number_input(
+height = st.slider(
     "Floor Height",
-    min_value=3,
-    max_value=999,
-    value=4, 
-    step=1,
+    min_value=3.0,
+    max_value=8.0,
+    value=4.0, 
+    step=0.5,
     key="floor-height")
 
-# room info
-n_window = st.slider(label='WWR - North',
-    min_value=0.00, 
-    max_value=0.95, 
-    step=0.05, 
-    value=0.35)
+with st.expander('WWR'):
+    # room info
+    n_window = st.slider(label='WWR - North',
+        min_value=0.00, 
+        max_value=0.95, 
+        step=0.05, 
+        value=0.35)
 
-e_window = st.slider(label='WWR - East',
-    min_value=0.00, 
-    max_value=0.95, 
-    step=0.05, 
-    value=0.35)
+    e_window = st.slider(label='WWR - East',
+        min_value=0.00, 
+        max_value=0.95, 
+        step=0.05, 
+        value=0.35)
 
-s_window = st.slider(label='WWR - South',
-    min_value=0.00, 
-    max_value=0.95, 
-    step=0.05, 
-    value=0.35)
-  
-w_window = st.slider(label='WWR - West',
-    min_value=0.00, 
-    max_value=0.95, 
-    step=0.05, 
-    value=0.35)
+    s_window = st.slider(label='WWR - South',
+        min_value=0.00, 
+        max_value=0.95, 
+        step=0.05, 
+        value=0.35)
+    
+    w_window = st.slider(label='WWR - West',
+        min_value=0.00, 
+        max_value=0.95, 
+        step=0.05, 
+        value=0.35)
 
-wwr = [n_window, e_window, 
-    s_window, w_window]
+    wwr = [n_window, e_window, 
+        s_window, w_window]
 
 # get rooms
 rooms = get_rooms(height=height, 
